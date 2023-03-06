b1bc63e0fecf4dc538323d379ec3615e70f60c23
FailureMan001 <papatsaro10@gmail.com>
2023-03-06 10:23:16 +0700

    เปลี่ยนเยอะเลย

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
-import Home from './components/Home';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import About from './components/About';
+import Home from './components/pages/Home';
+import Header from './components/pages/Header';
+import Footer from './components/pages/Footer';
+import About from './components/pages/About';
+import Blog from './components/pages/Blog';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
@@ -14,6 +16,11 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
+          <Route path='/blog' element={<Blog/>}/>
+          {/* <Route path='/blog-1' element={}/>
+          <Route path='/blog-2' element={}/>
+          <Route path='/blog-3' element={<}/> */}
+          <Route path='/contact' element={<Contact/>}/>
         </Routes>
       </BrowserRouter>
       <Footer/>
