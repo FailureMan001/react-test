1967d67936afc83fcc483ff9a210c50dd869f41a
FailureMan001 <papatsaro10@gmail.com>
2023-03-07 21:02:35 +0700

    อัพเดต About & SliderOfAbout

---
+++ src/components/layout/Sliderabout.js
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react'
+
+// * import : context
+import {ImageContext} from "../ImageContext";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-cube";
+import "swiper/css/pagination";
+
+import "./sliderTwo.css";
+
+// import required modules
+import { EffectCube, Pagination } from "swiper";
+
+const SliderOfAbout = () => {
+    const data = useContext(ImageContext);
+    const result = data.imageData.map((Element) => {
+      return Element.image;
+    });
+
+  return (
+    <>
+      <Swiper
+        effect={"cube"}
+        grabCursor={true}
+        cubeEffect={{
+          shadow: true,
+          slideShadows: true,
+          shadowOffset: 20,
+          shadowScale: 0.94,
+        }}
+        pagination={true}
+        modules={[EffectCube, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={result[14]} alt=''/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={result[15]} alt=''/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={result[13]} alt=''/>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  )
+}
+
+export default SliderOfAbout
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,114 @@
-import React from 'react'
+import React, { useContext } from "react";
+
+// * import : context
+import {ImageContext} from "../ImageContext";
+
+// * import : CSS
+import "../layout/about.css";
+
+// *import : Slider Of About
+import SliderOfAbout from "../layout/Sliderabout";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  const data = useContext(ImageContext);
+  const result = data.imageData.map((Element) => {
+    return Element.image;
+  });
 
-export default About
+  return (
+    <div className="container mx-auto -mt-3">
+      <div className="section bg-blue-100">
+        <div className="section-title">ABOUT US</div>
+        <div className="section-subtitle">
+          Exlore our rich and diverse portfolio
+        </div>
+      </div>
+
+      <div className="w-full bg-blue-200">
+        <div className="flex flex-wrap text-center text-white">
+          {/* About Column */}
+          <div className="flex justify-center w-full md:w-1/2 p-5 border-r border-orange-500">
+            <img src={result[25]} alt="" />
+          </div>
+          {/* About Column */}
+          <div className="w-full md:w-1/2 p-5 border-r border-blue-800 md:text-left">
+            <h1 className="my-6 ml-3 text-[100px] font-semibold">01</h1>
+
+            <p className="p-3 text-gray-900 text-2xl">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum,
+              dolor. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Earum, dolor.Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Earum, dolor.Lorem ipsum dolor sit amet consectetur,
+              adipisicing elit. Earum, dolor.Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Earum, dolor.Lorem ipsum dolor sit
+              amet consectetur, adipisicing elit. Earum, dolor.Lorem ipsum dolor
+              sit amet consectetur, adipisicing elit. Earum, dolor.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full bg-blue-200">
+        <div className="flex flex-wrap text-center text-white">
+          {/* About Column */}
+          <div className="w-full md:w-1/2 p-5 border-r border-blue-800 md:text-left">
+            <h1 className="my-6 ml-3 text-[100px] font-semibold">02</h1>
+
+            <p className="p-3 text-gray-900 text-2xl">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum,
+              dolor. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Earum, dolor.Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Earum, dolor.Lorem ipsum dolor sit amet consectetur,
+              adipisicing elit. Earum, dolor.Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Earum, dolor.Lorem ipsum dolor sit
+              amet consectetur, adipisicing elit. Earum, dolor.Lorem ipsum dolor
+              sit amet consectetur, adipisicing elit. Earum, dolor.
+            </p>
+          </div>
+          {/* About Column */}
+          <div className="flex justify-center w-full md:w-1/2 p-5 border-r border-orange-500">
+            <img src={result[10]} alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full bg-blue-200">
+        <div className="flex flex-wrap text-center text-white">
+          {/* About Column */}
+          <div className="flex justify-center w-full md:w-1/2 p-5 border-r border-orange-500">
+            <img src={result[11]} alt="" />
+          </div>
+          {/* About Column */}
+          <div className="w-full md:w-1/2 p-5 border-r border-blue-800 md:text-left">
+            <h1 className="my-6 ml-3 text-[100px] font-semibold">VARA HOME</h1>
+
+            <p className="p-3 text-gray-900 text-2xl">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum,
+              dolor. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Earum, dolor.Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Earum, dolor.Lorem ipsum dolor sit amet consectetur,
+              adipisicing elit. Earum, dolor.Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Earum, dolor.Lorem ipsum dolor sit
+              amet consectetur, adipisicing elit. Earum, dolor.Lorem ipsum dolor
+              sit amet consectetur, adipisicing elit. Earum, dolor.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* FACILITIES */}
+      <div className="section bg-blue-200">
+        
+        <div className="section-title">Facilities</div>
+        <div className="section-subtitle">
+          Exlore our rich and diverse portfolio
+        </div>
+        <SliderOfAbout/>
+      </div>
+      {/* END FACILITIES */}
+
+    </div>
+  );
+};
+
+export default About;
